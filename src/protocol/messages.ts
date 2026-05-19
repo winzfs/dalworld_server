5cc451ae076a7488ecb2c7399d6019b45867c240
winzfs <jazzhjm@gmail.com>
2026-05-19 21:44:24 +0900

    feat: include building update in protocol

--- src/protocol/messages.ts
+++ src/protocol/messages.ts
@@ -1,4 +1,4 @@
-import type { BuildingServerEvent, BuildDoorToggleRequest, BuildPlaceRequest, BuildRemoveRequest } from '../systems/building/BuildingTypes';
+import type { BuildingServerEvent, BuildDoorToggleRequest, BuildPlaceRequest, BuildRemoveRequest, BuildUpdateRequest } from '../systems/building/BuildingTypes';
 import type { GameWorldMap, WorldMapSourceRect } from '../worldMap/types';
 
 export type MovementKeys = {
@@ -81,7 +81,7 @@ export type PublicGameConfig = {
   gameplay: PublicGameplayConfig;
 };
 
-export type BuildingClientMessage = BuildPlaceRequest | BuildRemoveRequest | BuildDoorToggleRequest;
+export type BuildingClientMessage = BuildPlaceRequest | BuildUpdateRequest | BuildRemoveRequest | BuildDoorToggleRequest;
 
 export type ClientToServerMessage =
   | { type: 'hello'; name?: string }
