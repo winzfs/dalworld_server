837f2e33de0716a4bdce7312bccf06e811087f94
winzfs <jazzhjm@gmail.com>
2026-05-21 23:00:43 +0900

    Validate quest definitions during module initialization

--- src/systems/quest/QuestDefinitions.ts
+++ src/systems/quest/QuestDefinitions.ts
@@ -1,9 +1,12 @@
 import { CHAPTER_1_QUEST_DEFINITIONS } from './content/Chapter1QuestDefinitions';
 import type { QuestDefinition } from './QuestTypes';
+import { validateQuestDefinitions } from './QuestDefinitionValidator';
 
 export const QUEST_DEFINITIONS: QuestDefinition[] = [
   ...CHAPTER_1_QUEST_DEFINITIONS,
 ];
+
+validateQuestDefinitions(QUEST_DEFINITIONS);
 
 const QUEST_DEFINITION_BY_ID = new Map(QUEST_DEFINITIONS.map((quest) => [quest.id, quest]));
 
